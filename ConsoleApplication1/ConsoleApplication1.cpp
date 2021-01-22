2abc3c46a8c5513b982bc68a9b594bc0e650ec49
n1k113 <71322183+n1k113@users.noreply.github.com>
2021-01-22 12:58:57 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Car
+{
+private:
+    int distanse;
+public:
+    int get_distanse() {
+        return distanse;
+
+    }
+    void display() {
+        cout << distanse << endl;
+    }
+    void start() {
+        cout << "я еду" << endl;
+    };
+    void stop() {};
+
+    int drive(int howlong)
+    {
+
+        distanse = howlong * 60;
+        return distanse;
+    }
+};
+int main()
+{
+    Car Toyota;
+    int a;
+    cin >> a;
+    int c;
+    c = Toyota.drive(a);
+    Toyota.display();
+    return 0;
+}
